830d2550ab75aae2ce8dde533ccb03b747ef7c01
Yasin Mohammed <mohammedyasinym16720@gmail.com>
2026-07-05 14:01:06 +0300

    fix(schedule): typeorm migration done

--- src/business/schedule/dto/create-schedule.dto.ts
+++ src/business/schedule/dto/create-schedule.dto.ts
@@ -1,4 +1,11 @@
-import { IsNotEmpty, IsString, IsInt, Matches } from "class-validator";
+import {
+  IsNotEmpty,
+  IsString,
+  IsInt,
+  Min,
+  Max,
+  Matches,
+} from "class-validator";
 import { Type } from "class-transformer";
 
 export class CreateScheduleDto {
@@ -6,21 +13,24 @@ export class CreateScheduleDto {
   @IsInt()
   locationId: number;
 
-  @IsNotEmpty()
-  @IsString()
-  dayOfWeek: string;
+  // ISO 8601 weekday: 1 = Monday ... 7 = Sunday
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(7)
+  dayOfWeek: number;
 
   @IsNotEmpty()
   @IsString()
-  @Matches(/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/, {
-    message: "startTime must be in HH:MM format",
+  @Matches(/^([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/, {
+    message: "startTime must be in HH:MM or HH:MM:SS format",
   })
   startTime: string;
 
   @IsNotEmpty()
   @IsString()
-  @Matches(/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/, {
-    message: "endTime must be in HH:MM format",
+  @Matches(/^([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/, {
+    message: "endTime must be in HH:MM or HH:MM:SS format",
   })
   endTime: string;
 }
--- src/business/schedule/schedule.module.ts
+++ src/business/schedule/schedule.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { ScheduleService } from "./schedule.service";
 import { ScheduleController } from "./schedule.controller";
+import { Schedules } from "@/database/entities/schedules";
+import { Locations } from "@/database/entities/locations";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Schedules, Locations])],
   controllers: [ScheduleController],
   providers: [ScheduleService],
 })
